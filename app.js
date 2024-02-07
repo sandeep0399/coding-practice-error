d41830d15b2ed5732b7b2d26dd312e6d400cbf76
sandeep <sandeep.talari8999@gmail.com>
2024-02-07 06:03:32 +0000

    son

---
+++ app.js
@@ -0,0 +1,101 @@
+const express = require('express')
+const path = require('path')
+
+const {open} = require('sqlite')
+const sqlite3 = require('sqlite3')
+const app = express()
+app.use(express.json())
+const dbPath = path.join(__dirname, 'cricketTeam.db')
+
+let db = null
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    })
+    app.listen(3000, () => {
+      console.log('Server Running at http://localhost:3000/')
+    })
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`)
+    process.exit(1)
+  }
+}
+
+initializeDBAndServer()
+
+app.get('/players/', async (request, response) => {
+  const getplayers = `
+    SELECT
+      *
+    FROM
+      cricket_team
+    ORDER BY
+      player_id;`
+  const playersArray = await db.all(getplayers)
+  response.send(playersArray)
+})
+
+app.post('/players/', async (request, response) => {
+  const playerDetails = request.body
+  const {playerName, jerseyNumber, role} = playerDetails
+  const addPlayer = `
+    INSERT INTO
+      cricket_team  (player_name, jersey_number, role)
+    VALUES
+      (
+        '${playerName}',
+         ${jerseyNumber},
+         ${role},
+       
+      );`
+  const dbResponse = await db.run(addPlayer)
+  const playerId = dbResponse.lastID
+  response.send({playerId: playerId})
+  response.send('Player Added to Team')
+})
+
+app.get('/players/:playerId/', async (request, response) => {
+  const {playerId} = request.params
+  const getplayers = `
+    SELECT
+      *
+    FROM
+       cricket_team
+    WHERE
+     player_id= ${playerId};`
+  const player = await db.get(getplayers)
+  response.send(player)
+})
+
+app.put('/players/:playerId/', async (request, response) => {
+  const {playerId} = request.params
+  const playerDetails = request.body
+  const {playerName, jerseyNumber, role} = playerDetails
+  const updateBookQuery = `
+    UPDATE
+       cricket_team
+    SET
+      player_name='${playerName}',
+      jersey_number=${jerseyNumber},
+     role=${role}
+    WHERE
+      player_id = ${playerId};`
+  await db.run(updateBookQuery)
+  response.send('Player Details Updated')
+})
+
+app.delete('/players/:playerId/', async (request, response) => {
+  const {playerId} = request.params
+  const deleteplayer = `
+    DELETE FROM
+      cricket_team
+    WHERE
+      player_id = ${playerId};`
+  await db.run(deleteplayer)
+  response.send('Player Removed')
+})
+
+module.exports = app
